77f52c71b558f81c07c61e4e71b32435befbc2d5
PatrykDragon <patryk.dragon@outlook.com>
2020-11-12 00:18:27 +0100

    Added exercise 2 and 3

---
+++ exercises/Exercise_2_2.py
@@ -0,0 +1,24 @@
+""" Dany jest plik, gdzie w ka»dej linii znajduje si¦ saowo. Nale»y sprawdzi¢, czy dane
+
+saowo jest palindromem, a nast¦pnie do pliku csv zapisa¢ dla ka»dego saowa nast¦pu-
+j¡ce dane: liczba porz¡dkowa, saowo, wypisa¢ wszystkie znaki znajduj¡ce si¦ w tym
+
+saowie, wpisa¢ true  je»eli saowo jest palindromem i false, je»eli nie jest.
+ """
+
+import csv
+
+def check_palindrome(word):
+    if word == word[::-1]:
+        return True
+    else:
+        return False
+
+with open('1.txt', 'r', encoding='UTF-8') as file:
+    count = 1
+    with open('1.csv', 'w', newline= '') as write_file:
+        writer = csv.writer(write_file)
+        for line  in file:
+            line = line.strip()
+            writer.writerow([count,line,''.join(set(line)),check_palindrome(line)])
+            count+=1
---
+++ exercises/Exercise_2_3.py
@@ -0,0 +1,19 @@
+""" Do zadania doa¡czony zostaa plik csv z rzeczywistymi notowaniami spóaki indekso-
+wanej na gieadzie. Plik skaada si¦ z daty oraz zmiany procentowej w cenie instru-
+mentu wzgl¦dem poprzedniego odczytu. Bardzo cz¦sto w zestawieniach nansowych
+
+przedstawia si¦ zwroty za ostatni miesi¡c. Nale»y odczyta¢ dane, nast¦pnie wyzna-
+czy¢ ±redni¡ dla ka»dego z miesi¡ca i zapisa¢ ±rednie zmiany miesi¦czne w osobnym
+
+pliku. """
+
+from dataclasses import replace
+
+import pandas as pd
+
+
+data = pd.read_csv('1.csv',sep = ':', header = None)
+data.columns = ['Date','Percentage change']
+data['Date'] = data['Date'].str.slice_replace(4,7)
+grouped = data.groupby(['Date']).mean().round(2)
+grouped.to_csv('summary.csv',sep = ':')
